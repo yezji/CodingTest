dd32332355e8e888148bec5994008d8fd2884fcb
yezji <yeji.dev@gmail.com>
2020-10-10 22:31:50 +0900

    solve problem

--- exercise/boj/boj_2003_수들의합2.cpp
+++ exercise/boj/boj_2003_수들의합2.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int solution(int arr[], int &n, int &m) {
-    int answer = 0;
-    int i=0, j=0;
-    int sum = 0;
-
-    for (; i<n; i++) {
-        while (sum<m && j<n) {
-            sum += arr[j];
-            j++;
-        }
-        if (sum==m) answer++;
-        sum -= arr[i];
-    }
-    return answer;
-}
-
 int main() {
-    int arr[10010];
+    vector<int> arr(10010);
     int n, m;
     scanf("%d %d", &n, &m);
-
     for (int i=0; i<n; i++) scanf("%d", &arr[i]);
-    cout << solution(arr, n, m) << endl;
+    
+    int answer = 0;
+    int i = 0, j = 0;
+    int sum = arr[0];
+    while (j<n) {
+        if (sum<m) {
+            j++;
+            sum += arr[j];
+        }
+        else {
+            if (sum==m) answer++;
+            sum -= arr[i];
+            i++;
+        }
+    }
+    cout << answer << endl;
 
     return 0;
 }
